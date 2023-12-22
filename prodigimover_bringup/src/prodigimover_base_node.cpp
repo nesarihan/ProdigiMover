71476a6cfbacf52afb0e0de0f2512707d96d138d
nesarihan <demirneslihan85@gmail.com>
2023-12-22 12:34:12 +0300

    bringup package

--- prodigimover_bringup/src/prodigimover_base_node.cpp
+++ prodigimover_bringup/src/prodigimover_base_node.cpp
@@ -1,8 +1,8 @@
-#include "Prodigimover_base.h"
+#include "prodigimover_base.h"
 
 int main(int argc, char** argv )
 {
-    ros::init(argc, argv, "prodigimovern_base_node");
+    ros::init(argc, argv, "prodigimover_base_node");
     ProdigiMoverBase prodigimover;
     ros::spin();
     return 0;
